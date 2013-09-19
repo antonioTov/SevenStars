c358f1df540d1fd6fe77eb1c0446e5463b02de98
antonioTov <tov.ua@ukr.net>
2013-09-19 16:13:12 +0300

    http://sevenstars.tribalddb.com.ua/?f=add_project  new page

--- js/common.js
+++ js/common.js
@@ -141,6 +141,11 @@ function init()
     $(".scroll").mCustomScrollbar({
         scrollInertia:0
     });
+
+
+    if($('.ta-scroll').length > 0)
+        textarea_scroll_init();
+
 }
 
 //--------------------------------------------------------------/
@@ -225,4 +230,62 @@ function decor2()
     $('.decor_b1, .decor_b2').hide();
     $('.decor_b3').css({top: 126, height:$('.block_2').height()});
     $('.decor_b4').css({top: t, height: $('.footer').offset().top-t});
+}
+
+//-----------------------------------------------------------------/
+function textarea_scroll_init()
+{
+    //var textArea=$("textarea");
+    $("textarea").each(function(i){
+        textArea = $(this);
+        textArea.wrap("<div class='textarea-wrapper'/>");
+        var textAreaWrapper=textArea.parent(".textarea-wrapper");
+        textAreaWrapper.mCustomScrollbar({
+            scrollInertia:0,
+            advanced:{autoScrollOnFocus:false}
+        });
+        textAreaWrapper.css('height', textArea.height()+10);
+        var hiddenDiv=$(document.createElement("div")),
+            content=null;
+        hiddenDiv.addClass("hiddendiv").css('min-height', textArea.height());
+        $("body").prepend(hiddenDiv);
+        textArea.bind("keyup",function(e){
+            content=$(this).val();
+            var clength=content.length;
+            var cursorPosition=textArea.getCursorPosition();
+            content="<span>"+content.substr(0,cursorPosition)+"</span>"+content.substr(cursorPosition,content.length);
+            content=content.replace(/\n/g,"<br />");
+            hiddenDiv.html(content+"<br />");
+            $(this).css("height",hiddenDiv.height());
+            textAreaWrapper.mCustomScrollbar("update");
+            var hiddenDivSpan=hiddenDiv.children("span"),
+                hiddenDivSpanOffset=0,
+                viewLimitBottom=(parseInt(hiddenDiv.css("min-height")))-hiddenDivSpanOffset,
+                viewLimitTop=hiddenDivSpanOffset,
+                viewRatio=Math.round(hiddenDivSpan.height()+textAreaWrapper.find(".mCSB_container").position().top);
+            if(viewRatio>viewLimitBottom || viewRatio<viewLimitTop){
+                if((hiddenDivSpan.height()-hiddenDivSpanOffset)>0){
+                    textAreaWrapper.mCustomScrollbar("scrollTo",hiddenDivSpan.height()-hiddenDivSpanOffset);
+                }else{
+                    textAreaWrapper.mCustomScrollbar("scrollTo","top");
+                }
+            }
+        });
+
+    });
+
+    $.fn.getCursorPosition=function(){
+        var el=$(this).get(0),
+            pos=0;
+        if("selectionStart" in el){
+            pos=el.selectionStart;
+        }else if("selection" in document){
+            el.focus();
+            var sel=document.selection.createRange(),
+                selLength=document.selection.createRange().text.length;
+            sel.moveStart("character",-el.value.length);
+            pos=sel.text.length-selLength;
+        }
+        return pos;
+    }
 }
